adff7e488b7ccbca3b0e4826a7d789719fda8a52
Ali Helmi <ahelmi365@gmail.com>
2023-10-27 16:35:54 +0200

    use the verifyUser in login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,32 +1,31 @@
-import "./login.css"
-
+import "./login.css";
+import { BiSolidUserCircle } from "react-icons/bi";
+import VerifyUser from "../../components/verifiyUser/VerifyUser";
 const Login = () => {
   return (
-    <div className="login-container ">
-      
-      <div className="card form-container">
-      <h6 className="mb-3">Log In</h6>
-        <form>
-          <div className="form-group">
-            <label className="form-label" htmlFor="email">
-              Email
-            </label>
-            <input className="form-control" type="email" id="email" />
-          </div>
+    <VerifyUser Icon={BiSolidUserCircle} title="Log In">
+      <form>
+        <div className="form-group">
+          <label className="form-label" htmlFor="email">
+            Email
+          </label>
+          <input className="form-control" type="email" id="email" />
+        </div>
 
-          <div className="form-group mt-3">
-            <label className="form-label" htmlFor="password">
-              Password
-            </label>
-            <input className="form-control" type="password" id="password" />
-          </div>
+        <div className="form-group mt-3">
+          <label className="form-label" htmlFor="password">
+            Password
+          </label>
+          <input className="form-control" type="password" id="password" />
+        </div>
 
-          <div className="form-group mt-5">
-            <button className="btn btn-primary w-100" type="submit">Login</button>
-          </div>
-        </form>
-      </div>
-    </div>
+        <div className="form-group mt-5">
+          <button className="btn btn-primary w-100" type="submit">
+            Login
+          </button>
+        </div>
+      </form>
+    </VerifyUser>
   );
 };
 
