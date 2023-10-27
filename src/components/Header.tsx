a4c9b2461637106dc7a622d0f94644c70437bd08
Ali Helmi <ahelmi365@gmail.com>
2023-10-27 13:02:39 +0200

    add style to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,29 @@
 import { NavLink } from "react-router-dom";
 
 // css
-import "./header.css"
+import "./header.css";
 const Header = () => {
-
   return (
     <div className="row header mt-3">
       <ul className="nav-links">
-        <li className="nav-link">
-          <NavLink to="/" end>
-            Home
-          </NavLink>
-        </li>
-        <li className="nav-link">
-          <NavLink to="/faq">FAQ</NavLink>
-        </li>
-        <li className="nav-link">
-          <NavLink to="/contact">Contact</NavLink>
-        </li>
-        <li className="nav-link">
-          <NavLink to="/apex-dashboard">Apex-Charts</NavLink>
-        </li>
-        <li className="nav-link">
-          <NavLink to="/google-dashboard">Google-Charts</NavLink>
-        </li>
-        <li className="nav-link">
-          <NavLink to="/user/login"> Logout</NavLink>
-        </li>
-      
+        <NavLink to="/" end>
+          <li className="nav-link">Home</li>
+        </NavLink>
+        <NavLink to="/faq">
+          <li className="nav-link">FAQ</li>
+        </NavLink>
+        <NavLink to="/contact">
+          <li className="nav-link">Contact</li>
+        </NavLink>
+        <NavLink to="/apex-dashboard">
+          <li className="nav-link">Apex-Charts</li>
+        </NavLink>
+        <NavLink to="/google-dashboard">
+          <li className="nav-link">Google-Charts</li>
+        </NavLink>
+        <NavLink to="/user/login">
+          <li className="nav-link">Logout</li>
+        </NavLink>
       </ul>
     </div>
   );
