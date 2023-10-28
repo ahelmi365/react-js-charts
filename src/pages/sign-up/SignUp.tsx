bfcbc7458c7bfa321c9167c8f12489edf6953d18
Ali Helmi <ahelmi365@gmail.com>
2023-10-28 11:18:53 +0200

    add link to log-in

--- src/pages/sign-up/SignUp.tsx
+++ src/pages/sign-up/SignUp.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import VerifyUser from "../../components/verifiyUser/VerifyUser";
 import { AiOutlineUserAdd } from "react-icons/ai";
+import { Link } from "react-router-dom";
 const SignUp = () => {
   return (
     <VerifyUser Icon={AiOutlineUserAdd} title="Sign Up">
+      <p>
+        Do you have an account? <Link to="/user/login">Sign In</Link>
+      </p>
+
       <form>
         <div className="form-group mt-3">
           <label className="form-label" htmlFor="name">
@@ -49,6 +54,7 @@ const SignUp = () => {
             placeholder="Confirm Password"
           />
         </div>
+
         <div className="form-group mt-5">
           <button type="submit" className="btn btn-primary w-100">
             Submit
