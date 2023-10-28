102c030ee8cdbae19c1f91af1175ad40eac9c606
Ali Helmi <ahelmi365@gmail.com>
2023-10-28 11:18:17 +0200

    add login-container class

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,31 +1,38 @@
 import "./login.css";
 import { BiSolidUserCircle } from "react-icons/bi";
 import VerifyUser from "../../components/verifiyUser/VerifyUser";
+import { Link } from "react-router-dom";
 const Login = () => {
   return (
-    <VerifyUser Icon={BiSolidUserCircle} title="Log In">
-      <form>
-        <div className="form-group">
-          <label className="form-label" htmlFor="email">
-            Email
-          </label>
-          <input className="form-control" type="email" id="email" />
-        </div>
+    <div className="login-container">
+      <VerifyUser Icon={BiSolidUserCircle} title="Log In">
+        <p>
+          Create new account? <Link to="/user/sign-up">Sign-up</Link>
+        </p>
 
-        <div className="form-group mt-3">
-          <label className="form-label" htmlFor="password">
-            Password
-          </label>
-          <input className="form-control" type="password" id="password" />
-        </div>
+        <form>
+          <div className="form-group">
+            <label className="form-label" htmlFor="email">
+              Email
+            </label>
+            <input className="form-control" type="email" id="email" />
+          </div>
 
-        <div className="form-group mt-5">
-          <button className="btn btn-primary w-100" type="submit">
-            Login
-          </button>
-        </div>
-      </form>
-    </VerifyUser>
+          <div className="form-group mt-3">
+            <label className="form-label" htmlFor="password">
+              Password
+            </label>
+            <input className="form-control" type="password" id="password" />
+          </div>
+
+          <div className="form-group mt-5">
+            <button className="btn btn-primary w-100" type="submit">
+              Login
+            </button>
+          </div>
+        </form>
+      </VerifyUser>
+    </div>
   );
 };
 
